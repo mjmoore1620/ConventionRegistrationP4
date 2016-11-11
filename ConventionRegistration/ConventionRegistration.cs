87698dce914bca294e3cc5daf93f5b706d082aa5
Allison <iveyas@etsu.edu>
2016-11-11 18:34:35 -0500

    Events

--- ConventionRegistration/ConventionRegistration.cs
+++ ConventionRegistration/ConventionRegistration.cs
@@ -9,6 +9,36 @@ namespace ConventionRegistration
     class ConventionRegistration
     {
 
+        private TimeSpan TimeAtRegistration;
+        private DateTime ArrivalTime;
+        private DateTime DepartureTime;
+        private TimeSpan durationTime; 
+        public DateTime Arrival
+        {
+            get {return ArrivalTime; }
+            set {ArrivalTime = value; }
+
+        }
+        
+        public DateTime Departure
+        {
+            get {return DepartureTime; }
+            set {DepartureTime = value; }
+        }
+        public TimeSpan duration
+        {
+            get {return durationTime; }
+            set {durationTime = value; }
+        }
+       
+
+        public TimeSpan MyProperty
+        {
+            get { return TimeAtRegistration; }
+            set { TimeAtRegistration = value; }
+        }
+
+
     }
 
     
--- ConventionRegistration/Events.cs
+++ ConventionRegistration/Events.cs
@@ -9,9 +9,45 @@ namespace ConventionRegistration
     /// <summary>
     /// 
     /// </summary>
-    class Events
+    enum EVENTTYPE {  ENTER, LEAVE }
+    class Evnts : IComparable
     {
+        public EVENTTYPE Type { get; set; }
         public DateTime Time { get; set; }
+        public int Patron { get; set; }
+
+        public Evnts()
+        {
+            Type = EVENTTYPE.ENTER;
+            Time = DateTime.Now;
+            Patron = -1;
+        }
+
+        public Evnts (EVENTTYPE type, DateTime time, int patron)
+        {
+            Type = type;
+            Time = time;
+            Patron = patron;
+        }
+
+        public override string ToString()
+        {
+            String str = "";
+            str += String.Format("Patron {0} ", Patron.ToString().PadLeft(3));
+            str += Type + "'s";
+            str += String.Format(" at {0}", Time.ToShortTimeString().PadLeft(8));
+
+            return str;
+        }
+
+        public int CompareTo(Object obj)
+        {
+            if (!(obj is Evnts))
+                throw new ArgumentException("The argument is not an Event Object");
+
+            Evnts e = (Evnts)obj;
+            return (e.Time.CompareTo(Time));
+        }
 
 
 
