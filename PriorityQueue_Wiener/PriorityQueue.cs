d16608bf416f15f0c572697d5f2e211e4fde86d6
Allison <iveyas@etsu.edu>
2016-11-13 15:01:08 -0500

    changes

--- PriorityQueue_Wiener/PriorityQueue.cs
+++ PriorityQueue_Wiener/PriorityQueue.cs
@@ -7,14 +7,32 @@ using System.Threading.Tasks;
 namespace PriorityQueue_Wiener
 {
     //this is a change
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="PriorityQueue_Wiener.IPriorityQueue{T}" />
     public class PriorityQueue<T> : IPriorityQueue<T> where T: IComparable
     {
         //fields and properties
+        /// <summary>
+        /// The top
+        /// </summary>
         private Node top;               //reference to the top of the PQ
 
+        /// <summary>
+        /// Gets or sets the count.
+        /// </summary>
+        /// <value>
+        /// The count.
+        /// </value>
         public int Count { get; set; }
 
         //Add an item to the PQ
+        /// <summary>
+        /// Enqueues the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
         public void Enqueue(T item)
         {
             if (Count == 0)
@@ -44,6 +62,11 @@ namespace PriorityQueue_Wiener
         }
 
         //Remove an item from the priority queue and discard it
+        /// <summary>
+        /// Dequeues this instance.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidOperationException">Cannot remove from empty queue.</exception>
         public T Dequeue()
         {
             if (IsEmpty())
@@ -65,12 +88,20 @@ namespace PriorityQueue_Wiener
         }
 
         //Make PQ empty
+        /// <summary>
+        /// Clears this instance.
+        /// </summary>
         public void Clear()
         {
             top = null;
         }
 
         //Retrieve the top item on the PQ
+        /// <summary>
+        /// Peeks this instance.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidOperationException">Cannot obtain top of priority queue.</exception>
         public T Peek()
         {
             if (!IsEmpty())
@@ -80,18 +111,46 @@ namespace PriorityQueue_Wiener
         }
 
         //asks whether the PQ is empty
+        /// <summary>
+        /// Determines whether this instance is empty.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this instance is empty; otherwise, <c>false</c>.
+        /// </returns>
         public bool IsEmpty()
-        {
+        { 
+
             return Count == 0;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <seealso cref="PriorityQueue_Wiener.IPriorityQueue{T}" />
         private class Node
         {
             //properties
+            /// <summary>
+            /// Gets or sets the item.
+            /// </summary>
+            /// <value>
+            /// The item.
+            /// </value>
             public T Item { get; set; }
+            /// <summary>
+            /// Gets or sets the next.
+            /// </summary>
+            /// <value>
+            /// The next.
+            /// </value>
             public Node Next { get; set; }
 
             //constructor
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Node"/> class.
+            /// </summary>
+            /// <param name="value">The value.</param>
+            /// <param name="link">The link.</param>
             public Node(T value, Node link)
             {
                 Item = value;
